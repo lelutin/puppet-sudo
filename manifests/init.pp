8a8719c7a1b172535711e904105b4c18501c8bbb
mh <mh@immerda.ch>
2009-01-26 21:23:46 +0000

    if $sudo_deploy_sudoers is set, we deploy now a sudoers file as well

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,32 @@
 # GPLv3
 
 class sudo {
-    case $operatingsystem {
+    case $kernel {
+        linux: { include sudo::linux }
         default: { include sudo::base }
     }
 }
 
 class sudo::base {
+    if $sudo_deploy_sudoers {
+      file{'/etc/sudoers':
+        source => [ "puppet://$source/files/sudo/sudoers/${fqdn}/sudoers",
+                    "puppet://$source/files/sudo/sudoers/sudoers",
+                    "puppet://$source/sudo/sudoers/${operatingsystem}/sudoers",
+                    "puppet://$source/sudo/sudoers/sudoers" ],
+        owner => root, group => 0, mode => 0440;
+      }
+    }
+}
+
+class sudo::linux inherits sudo::base {
     package{'sudo':
         ensure => installed,
+    }
+
+    if $sudo_deploy_sudoers {
+      File['/etc/sudoers']{
+        require => Package['sudo'],
+      }
     }
 }
