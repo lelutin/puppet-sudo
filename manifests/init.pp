d5a1bcaf37caaddf7dc1545d63bdf36d7825c0e5
Guillaume Beaulieu <gbeaulieu@puppet0.koumbit.net>
2013-04-22 16:37:17 -0400

    Some fucking class that have some fukcing class

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,4 +18,13 @@ class koumbit::sudo {
     }
 }
 
-
+define koumbit::sudo::extra-acces($user, $access) {
+     file { "/etc/sudoers.d/$user":
+       ensure  => 'present',
+       owner   => root,
+       group   => admin,
+       mode    => 640,
+       content => "$user	$access",
+       notify  => Service[apache2]
+     }
+}
