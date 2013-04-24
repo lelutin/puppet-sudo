74e867802cd18299da25a4b0876c1556a8f1e220
Guillaume Beaulieu <gbeaulieu@puppet0.koumbit.net>
2013-04-24 13:44:04 -0400

    Initial commit !

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,13 +18,24 @@ class koumbit::sudo {
     }
 }
 
-define koumbit::sudo::extra-access($user, $access) {
-     file { "/etc/sudoers.d/$user":
-       ensure  => 'present',
+define sudo-user-alias ($ensure = 'present', $groupname, $members) {
+     file { "/etc/sudoers.d/00-user_alias-$title":
+       ensure  => $ensure,
        owner   => root,
-       group   => admin,
-       mode    => 640,
-       content => "$user	$access",
-       notify  => Service[apache2]
+       group   => root,
+       mode    => 440,
+       content => "# THIS FILE IS MANAGED BY PUPPET !\n\nUser_Alias $groupname = $members\n",
      }
 }
+
+define sudo-extra-access($ensure = 'present', $user, $access) {
+     file { "/etc/sudoers.d/01-user_access-$title":
+       ensure  => $ensure,
+       owner   => root,
+       group   => root,
+       mode    => 440,
+       content => "# THIS FILE IS MANAGED BY PUPPET !\n\n$user        $access\n",
+     }
+     
+}
+
