4aa05ebb491388394f78e68cca5a587017a23b50
Antoine Beaupré <anarcat@koumbit.org>
2013-04-24 14:29:04 -0400

    files were reversed

--- manifests/user-alias.pp
+++ manifests/user-alias.pp
@@ -1,10 +1,9 @@
-define sudo::extra-access($ensure = 'present', $user, $access) {
-     file { "/etc/sudoers.d/01-user_access-$title":
+define sudo::user-alias ($ensure = 'present', $groupname, $members) {
+     file { "/etc/sudoers.d/00-user_alias-$title":
        ensure  => $ensure,
        owner   => root,
        group   => root,
        mode    => 440,
-       content => "# THIS FILE IS MANAGED BY PUPPET !\n\n$user        $access\n",
+       content => "# THIS FILE IS MANAGED BY PUPPET !\n\nUser_Alias $groupname = $members\n",
      }
-     
 }
