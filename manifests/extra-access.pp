a5dc3d88fd9695942fbe62ffe7517a27e019e434
Antoine Beaupré <anarcat@koumbit.org>
2013-04-24 15:23:55 -0400

    add back the user parameter
    it seems we can discriminate on the name parameter, it gives us duplicates as both title and name need to be unique

--- manifests/extra-access.pp
+++ manifests/extra-access.pp
@@ -1,9 +1,12 @@
-define sudo::extra-access($ensure = 'present', $access = 'ALL=(ALL) ALL') {
-     file { "/etc/sudoers.d/01-user_access-$title":
+define sudo::extra-access($ensure = 'present', $user = undef, $access = 'ALL=(ALL) ALL') {
+     if $user == undef {
+       $user = $name
+     }
+     file { "/etc/sudoers.d/01-user_access-${name}":
        ensure  => 'present',
        owner   => root,
        group   => admin,
        mode    => 640,
-       content => "# THIS FILE IS MANAGED BY PUPPET !\n\n$name        $access\n",
+       content => "# THIS FILE IS MANAGED BY PUPPET !\n\n${user}        ${access}\n",
      }
 }
