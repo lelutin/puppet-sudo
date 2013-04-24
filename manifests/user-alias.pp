d996ff97977a69e6060dd58574c876f390a266e3
Antoine Beaupré <anarcat@koumbit.org>
2013-04-24 16:04:45 -0400

    style

--- manifests/user-alias.pp
+++ manifests/user-alias.pp
@@ -1,10 +1,10 @@
 define sudo::user-alias ($ensure = 'present', $members) {
     # this is 00-prefixed so that it's defined before the other definitions
-    file { "/etc/sudoers.d/00-user_alias-$title":
+    file { "/etc/sudoers.d/00-user_alias-${title}":
        ensure  => $ensure,
        owner   => root,
        group   => root,
        mode    => 440,
-       content => "# THIS FILE IS MANAGED BY PUPPET !\n\nUser_Alias $name = $members\n",
+       content => "# THIS FILE IS MANAGED BY PUPPET !\n\nUser_Alias ${name} = ${members}\n",
      }
 }
