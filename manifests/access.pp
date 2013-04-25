9c4d6fc6ec43c46081a2765512c6e453478e9fd7
Gabriel Filion <gabster@lelutin.ca>
2013-04-25 18:58:04 -0400

    lint manifests
    respect style from best practices. this makes the code a lot easier to
    read.
    
    also change groups from "root" to "0" to make them more portable (e.g.
    in freebsd, the admin group is "wheel" but has gid "0")

--- manifests/access.pp
+++ manifests/access.pp
@@ -1,13 +1,17 @@
-define sudo::access($ensure = 'present', $user = undef, $access = 'ALL=(ALL) ALL') {
-     if $user == undef {
-       $user = $name
-     }
-     # there shouldn't be a dot in those filenames!
-     file { "/etc/sudoers.d/01-user_access-${title}":
-       ensure  => 'present',
-       owner   => root,
-       group   => root,
-       mode    => 0440,
-       content => "# THIS FILE IS MANAGED BY PUPPET !\n${user}        ${access}\n",
-     }
+define sudo::access (
+  $ensure = 'present',
+  $user = undef,
+  $access = 'ALL=(ALL) ALL'
+) {
+  if $user == undef {
+    $user = $name
+  }
+  # there shouldn't be a dot in those filenames!
+  file { "/etc/sudoers.d/01-user_access-${title}":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 0,
+    mode    => '0440',
+    content => "# THIS FILE IS MANAGED BY PUPPET !\n${user}        ${access}\n",
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,19 @@
 class sudo {
-    package { 'sudo': ensure => installed, }
+  package { 'sudo': ensure => installed, }
 
-    file { "/etc/sudoers":
-        mode => 440, owner => root, group => 0,
-        require => Package['sudo'],
-        source => [ "puppet:///secrets/sudoers.$fqdn", "puppet:///secrets/sudoers" ],
+  file { '/etc/sudoers':
+    source  => ["puppet:///secrets/sudoers.${::fqdn}",
+                'puppet:///secrets/sudoers' ],
+    mode    => '0440',
+    owner   => 'root',
+    group   => 0,
+    require => Package['sudo'],
+  }
+  if $::operatingsystem == 'FreeBSD' {
+    File['/etc/sudoers'] {
+      path   => '/usr/local/etc/sudoers',
+      source => [ "puppet:///secrets/sudoers.${::fqdn}",
+                  'puppet:///secrets/sudoers.FreeBSD' ],
     }
-    if $operatingsystem == 'FreeBSD' {
-        File["/etc/sudoers"] {
-            path => "/usr/local/etc/sudoers",
-            source => [ "puppet:///secrets/sudoers.$fqdn", "puppet:///secrets/sudoers.FreeBSD" ],
-        }
-    }
+  }
 }
