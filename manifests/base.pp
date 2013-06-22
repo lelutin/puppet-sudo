1f777fc52d7afdc2a0bac7868082f3550eafd473
Antoine Beaupré <anarcat@koumbit.org>
2013-06-22 13:04:34 -0400

    deploy the sudoers directory even if we don't overwrite sudoers

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,15 +1,18 @@
 class sudo::base {
     if $sudo::deploy_sudoers {
-      file{
+      file {
         '/etc/sudoers':
           source => [ "puppet:///modules/site_sudo/sudoers/${::fqdn}/sudoers",
                       "puppet:///modules/site_sudo/sudoers/sudoers",
                       "puppet:///modules/sudo/sudoers/${::operatingsystem}/sudoers",
                       "puppet:///modules/sudo/sudoers/sudoers" ],
           owner => root, group => 0, mode => 0440;
-        $sudo::dir:
-          ensure => directory,
-          owner => root, group => 0, mode => 0550;
       }
     }
+    file {
+      $sudo::dir:
+        ensure => directory,
+        owner => root, group => 0, mode => 0550;
+    }
+
 }
