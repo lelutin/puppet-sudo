6f8ba040e56b4f81377566d58a195b8664e8c8aa
Micah Anderson <micah@riseup.net>
2013-06-22 14:11:30 -0400

    lint the init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,24 +7,30 @@ class sudo(
   $path = $sudo::params::path,
   $dir = $sudo::params::dir
 ) inherits sudo::params {
+
   package {'sudo':
     ensure => installed,
   }
+
   if $sudo::deploy_sudoers {
     file {
       $sudo::path:
-        source => [ "puppet:///modules/site_sudo/sudoers/${::fqdn}/sudoers",
-                    "puppet:///modules/site_sudo/sudoers/sudoers",
-                    "puppet:///modules/sudo/sudoers/${::operatingsystem}/sudoers",
-                    "puppet:///modules/sudo/sudoers/sudoers" ],
+        source  => [ "puppet:///modules/site_sudo/sudoers/${::fqdn}/sudoers",
+                     'puppet:///modules/site_sudo/sudoers/sudoers',
+                     "puppet:///modules/sudo/sudoers/${::operatingsystem}/sudoers",
+                     'puppet:///modules/sudo/sudoers/sudoers' ],
         require => Package['sudo'],
-        owner => root, group => 0, mode => 0440;
+        owner   => root,
+        group   => 0,
+        mode    => '0440';
     }
   }
   file {
     $sudo::dir:
-      ensure => directory,
+      ensure  => directory,
       require => Package['sudo'],
-      owner => root, group => 0, mode => 0550;
+      owner   => root,
+      group   => 0,
+      mode    => '0550';
   }
 }
