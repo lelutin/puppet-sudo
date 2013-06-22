81c6a5f6d02155fad61d338262b1dd1304231cec
Antoine Beaupré <anarcat@koumbit.org>
2013-06-22 12:58:45 -0400

    style

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,15 +1,15 @@
 class sudo::base {
     if $sudo::deploy_sudoers {
-      file{'/etc/sudoers':
-        source => [ "puppet:///modules/site_sudo/sudoers/${::fqdn}/sudoers",
-                    "puppet:///modules/site_sudo/sudoers/sudoers",
-                    "puppet:///modules/sudo/sudoers/${::operatingsystem}/sudoers",
-                    "puppet:///modules/sudo/sudoers/sudoers" ],
-        owner => root, group => 0, mode => 0440;
-      }
-      file { $sudo::dir:
-        ensure => directory,
-        owner => root, group => 0, mode => 0550;
+      file{
+        '/etc/sudoers':
+          source => [ "puppet:///modules/site_sudo/sudoers/${::fqdn}/sudoers",
+                      "puppet:///modules/site_sudo/sudoers/sudoers",
+                      "puppet:///modules/sudo/sudoers/${::operatingsystem}/sudoers",
+                      "puppet:///modules/sudo/sudoers/sudoers" ],
+          owner => root, group => 0, mode => 0440;
+        $sudo::dir:
+          ensure => directory,
+          owner => root, group => 0, mode => 0550;
       }
     }
 }
